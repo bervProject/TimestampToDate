44e4462599903cb4d7a7ed46ebaf86d59ce17bea
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-08-01 20:31:18 +0700

    Initial Projects

---
+++ TimestampToDate/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TimestampToDate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string timestampString = source.Text;
+            string result = ConvertTimestampToDateTimeString(timestampString);
+            destination.Text = result;
+        }
+
+        private static string ConvertTimestampToDateTimeString(string timestamp)
+        {
+            bool parsable = long.TryParse(timestamp, out long timestampLong);
+            if (!parsable)
+            {
+                return string.Empty;
+            }
+            try
+            {
+                DateTimeOffset dateTime = DateTimeOffset.FromUnixTimeMilliseconds(timestampLong);
+                return dateTime.ToLocalTime()
+                               .ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return string.Empty;
+            }
+
+        }
+    }
+}
