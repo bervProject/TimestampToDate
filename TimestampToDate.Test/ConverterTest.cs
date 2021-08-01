e95e1300f0a4a139682dfd6de491361bfde17f00
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-08-01 20:57:27 +0700

    Add Default  Culture

--- TimestampToDate.Test/ConverterTest.cs
+++ TimestampToDate.Test/ConverterTest.cs
@@ -1,16 +1,22 @@
+using System.Globalization;
 using TimestampToDate.Lib.Utils;
 using Xunit;
 
 namespace TimestampToDate.Test
 {
-    public class ConvertertTest
+    public class ConverterTest
     {
+        public ConverterTest()
+        {
+            CultureInfo.CurrentCulture = new CultureInfo("id-ID");
+        }
+
         [Fact]
         public void ExpectedConverted()
         {
             string timestamp = "1624801432170";
             string result = Converter.ConvertTimestampToDateTimeString(timestamp);
-            string expectedDate = "27/06/2021 20:43:52 +07:00";
+            string expectedDate = "27/06/2021 20.43.52 +07:00";
             Assert.Equal(expectedDate, result);
         }
 
